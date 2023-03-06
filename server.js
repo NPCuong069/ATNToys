4fac4a4776c7950c92c2bbf4ee4f0fa0176777be
92176099 <65901079+NPCuong069@users.noreply.github.com>
2023-03-06 16:56:27 +0700

    My  commit

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 // server.js
 //console.log('May Node be with you')
-
+const crypto = require("crypto")
 const express = require('express');
 const bodyParser= require('body-parser')
 const app = express();
-
+const mongodb = require('mongodb');
 const MongoClient = require('mongodb').MongoClient
 
-const connectionString = 'mongodb+srv://92176099:Bunoi123@cluster0.eohy8jn.mongodb.net/test'
+const connectionString = 'mongodb+srv://92176099:Bunoi123@cluster0.eohy8jn.mongodb.net/atntoys'
 
 // (0) CONNECT: server -> connect -> MongoDB Atlas 
 MongoClient.connect(connectionString, { useUnifiedTopology: true })
@@ -16,8 +16,8 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
         
         // (1a) CREATE: client -> create -> database -> 'star-wars-quotes'
         // -> create -> collection -> 'quotes'
-        const db = client.db('star-wars-quotes')
-        const quotesCollection = db.collection('quotes')
+        const db = client.db('atn-toys')
+        const toysCollection = db.collection('toys')
         
         // To tell Express to EJS as the template engine
         app.set('view engine', 'ejs') 
@@ -35,7 +35,7 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
         // -> server -> '/' -> collection -> 'quotes' -> find() 
         // -> results -> index.ejs -> client
         app.get('/', (req, res) => {
-            db.collection('quotes').find().toArray()
+            db.collection('toys').find().toArray()
                 .then(results => {
 
                     // results -> server -> console
@@ -43,15 +43,41 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
                     
                     // results -> index.ejs -> client -> browser 
                     // The file 'index.ejs' must be placed inside a 'views' folder BY DEFAULT
-                    res.render('index.ejs', { quotes: results })
+                    res.render('index.ejs', { toys: results })
                 })
                 .catch(/* ... */)
         })
+        app.get('/create', (req, res) => {
+            db.collection('toys').find().toArray()
+                .then(results => {
 
+                    // results -> server -> console
+                    console.log(results)
+                    
+                    // results -> index.ejs -> client -> browser 
+                    // The file 'index.ejs' must be placed inside a 'views' folder BY DEFAULT
+                    res.render('create.ejs', { toys: results })
+                })
+                .catch(/* ... */)
+        })
+        app.get('/home', (req, res) => {
+            db.collection('toys').find().toArray()
+                .then(results => {
+
+                    // results -> server -> console
+                    console.log(results)
+                    
+                    // results -> index.ejs -> client -> browser 
+                    // The file 'index.ejs' must be placed inside a 'views' folder BY DEFAULT
+                    res.render('home.ejs', { toys: results })
+                })
+                .catch(/* ... */)
+        })
         // (1b) CREATE: client -> index.ejs -> data -> SUBMIT 
         // -> post -> '/quotes' -> collection -> insert -> result
-        app.post('/quotes', (req, res) => {
-            quotesCollection.insertOne(req.body)
+        app.post('/toys', (req, res) => {
+            req.body.toyId=crypto.randomUUID().toString;
+            toysCollection.insertOne(req.body)
             .then(result => {
                 
                 // results -> server -> console
@@ -62,10 +88,47 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
              })
             .catch(error => console.error(error))
         })
-        
-        // server -> listen -> port -> 3000
-        app.listen(3000, function() {
-            console.log('listening on 3000')
+
+        app.post('/delete', (req, res) => {
+            toysCollection.deleteOne(
+                { _id: new mongodb.ObjectId(req.body._id) }
+            )
+            .then(result => {
+                res.redirect('/')
+            })
+            .catch(error => console.error(error))
+        })
+        app.post('/update', (req, res) => {
+            toysCollection.findOne(
+                { _id: new mongodb.ObjectId(req.body._id) }
+            )
+            .then(result => {
+                console.log(result)
+                res.render('update.ejs', { toy: result })
+            })
+            .catch(error => console.error(error))
+        })
+        app.post('/updateData', (req, res) => {
+            toysCollection.findOneAndUpdate(
+                { _id: new mongodb.ObjectId(req.body._id) },
+                {
+                    $set: {
+                        name: req.body.name,
+                        price:req.body.price,
+                        description: req.body.description,
+                        imageurl: req.body.imageurl
+                    }
+                }
+            )
+            .then(result => {
+                console.log(result)
+                res.redirect('/')
+               })
+            .catch(error => console.error(error))
+        })
+        const PORT = process.env.PORT || 4000;
+        app.listen(PORT, function() {
+            console.log('listening on 4000')
         })
     })
 
